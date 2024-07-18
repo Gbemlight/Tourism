f9aa4ad565ffd900980b604297522ac7ea80aabe
Gbemlight <adeyanjusunday114@gmail.com>
2024-07-18 16:53:46 +0100

    update

--- src/Pages/AboutUs/AboutUs.jsx
+++ src/Pages/AboutUs/AboutUs.jsx
@@ -4,6 +4,21 @@ import Navbar from "../../Components/Navbar/Navbar";
 // import Logo from "../../assets/logo(1).svg";
 import bgstyle from "../../assets/bgstyle.png";
 import boatimg from "../../assets/boatimg.png";
+import globe from "../../assets/globe.png";
+import videx from "../../assets/videx.png";
+import { FaPlayCircle } from "react-icons/fa";
+import popular from "../../assets/popular.png";
+import elip1 from "../../assets/elip1.png";
+import elip2 from "../../assets/elip2.png";
+import elip3 from "../../assets/elip3.png";
+import bowl from "../../assets/bowl.png";
+import sight1 from "../../assets/sight1.png";
+import sight2 from "../../assets/sight2.png";
+import sight3 from "../../assets/sight3.png";
+import sight4 from "../../assets/sight4.png";
+import sight5 from "../../assets/sight5.png";
+import sight6 from "../../assets/sight6.png";
+import sight7 from "../../assets/sight7.png";
 
 const AboutUs = () => {
   return (
@@ -23,17 +38,75 @@ const AboutUs = () => {
           <h5>
             We Provide You Best <br /> Sightseeing Europe Tours
           </h5>
-          <p>
+          <h6>
             Discover the unparalleled beauty of Europe with our meticulously{" "}
             <br /> crafted sightseeing tours. Immerse yourself in the rich
             history and <br /> culture of iconic cities like Paris, Rome, and
             Barcelona.
-          </p>
-           <button>View Packages</button>
+          </h6>
+          <button>View Packages</button>
         </div>
         <div className="rightimg">
-          <img src={bgstyle} alt="img" />
-          <img src={boatimg} alt="img" />
+          <img className="bgcol" src={bgstyle} alt="img" />
+          <img className="bgcol2" src={boatimg} alt="img" />
+          <img className="bgcol3" src={globe} alt="img" />
+        </div>
+      </div>
+      <div className="video">
+        <div className="mp4">
+          <img src={videx} alt="video" />
+        </div>
+        <div className="toplay">
+          <h5>Wanderlust</h5>
+        </div>
+        <div>
+          <FaPlayCircle className="playit" />
+        </div>
+      </div>
+      <div className="populartour">
+        <div className="popularimg">
+          <img src={popular} alt="img" />
+        </div>
+        <div className="populartext">
+          <p>TREND</p>
+          <h5>Our Popular Tour Plans</h5>
+          <h6>
+            Explore our selection of popular tour plans, meticulously designed
+            to <br /> offer you unforgettable experiences in some of the world's
+            most <br /> captivating destinations.
+          </h6>
+          <div className="elips">
+            <img src={elip1} alt="" />
+            <img src={elip2} alt="" />
+            <img src={elip3} alt="" />
+          </div>
+        </div>
+        <div className="dish">
+          <img src={bowl} alt="image" />
+        </div>
+      </div>
+      <div className="imageprove">
+        <div className="imageprovetext">
+          <p>EXPLORE MORE</p>
+          <h6>Our International Packages</h6>
+        </div>
+        <div className="picsflex">
+          <div className="subflex">
+            <img src={sight1} alt="image" />
+
+            <img src={sight2} alt="image" />
+
+            <img src={sight4} alt="image" />
+
+            <img src={sight5} alt="image" />
+
+            <img src={sight6} alt="image" />
+
+            <img src={sight7} alt="image" />
+          </div>
+          <div className="flexsub">
+            <img src={sight3} alt="image" />
+          </div>
         </div>
       </div>
     </div>
